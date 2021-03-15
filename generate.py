048eba176aa4375153f77d7d909f8e24743ca2f2
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2021-03-15 18:40:20 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,41 +6,69 @@ import telegram
 from datetime import datetime, timedelta
 import time
 import os.path
+    
+def readSince():
+    since = int(time.mktime((datetime.now() - timedelta(1)).timetuple()))
 
-consumer_key = sys.argv[1]
-access_token = sys.argv[2]
-telegram_bot_api_key = sys.argv[3]
+    file_name = "temp"
+    if os.path.exists(file_name):
+        with open(file_name, "r") as f:
+            since = int(f.readline()) + 1
+			
+    return since
+            
+def saveSince(since):
+    file_name = "temp"
+    with open(file_name, "w") as f:
+        f.write(str(since))
+            
+def loadArticles(since):
+    response = requests.get(f'https://getpocket.com/v3/get?consumer_key={consumer_key}&access_token={access_token}&state=archive&detailType=complete&since={since}&sort=oldest&tag=newsletter&count=1').content
 
-since = int(time.mktime((datetime.now() - timedelta(1)).timetuple()))
-
-file_name = "temp"
-if os.path.exists(file_name):
-    with open(file_name, "r") as f:
-        since = int(f.readline())
-
-bot = telegram.Bot(token=telegram_bot_api_key)
-
-response = requests.get(f'https://getpocket.com/v3/get?consumer_key={consumer_key}&access_token={access_token}&state=archive&detailType=complete&since={since}&sort=oldest&tag=newsletter').content
-
-data = json.loads(response)
-
-def mapItem(item):
-    title = item['resolved_title']
+    return json.loads(response)
+    
+def mapArticles(data):
+    items = list(map(lambda kv: kv[1], data["list"].items()))
+    items = list(map(lambda x: mapArticle(x), items))
+    return items
+    
+def mapArticle(item):
+    title = item['given_title']
     url = item['resolved_url']
     description = item['excerpt']
     authors = ",".join(list(map(lambda kv: kv[1]['name'], item.get("authors", {}).items())))
-    if authors:
-        return f"✍️ {authors}\n🏷️ [{title}]({url})\n📜 {description}"
-    else:
-        return f"🏷️ [{title}]({url})\n📜 {description}"
-
-if 'list' in data and len(data["list"]) > 0:
-    with open(file_name, "w") as f:
-        f.write(str(int(time.mktime(datetime.now().timetuple()))))
-
-    items = list(map(lambda kv: kv[1], data["list"].items()))
-    items = list(filter(lambda x: int(x['time_read']) >= since, items))
-    items = list(map(lambda x: mapItem(x), items))
+    time_updated = item['time_updated']
     
-    for item in items:
-        bot.send_message(chat_id="@krossovochkin_newsletter", text=item, parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+    result = ""
+    if authors:
+        result +=  f"✍️ {authors}\n"
+    
+    result += f"🏷️ [{title}]({url})"
+    
+    if description:
+        result += f"\n📜 {description}"
+        
+    return (result, time_updated)
+
+if __name__ == "__main__":
+    consumer_key = sys.argv[1]
+    access_token = sys.argv[2]
+    telegram_bot_api_key = sys.argv[3]
+
+    since = readSince()
+
+    bot = telegram.Bot(token=telegram_bot_api_key)
+
+    data = loadArticles(since)
+
+    if 'list' in data and len(data["list"]) > 0:
+        items = mapArticles(data)
+        
+        for item in items:
+            try:
+                bot.send_message(chat_id="@krossovochkin_newsletter", text=item[0], parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+                # print(item[0].encode("utf-8"))
+                # print(item[1])
+                saveSince(item[1])
+            except Exception as e:
+                print(f"Error: {e}")
