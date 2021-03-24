c8b36480a8eb951a9603e41d54537a9b931b4a00
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2021-03-24 18:00:59 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -14,7 +14,7 @@ def readSince():
     if os.path.exists(file_name):
         with open(file_name, "r") as f:
             since = int(f.readline()) + 1
-			
+            
     return since
             
 def saveSince(since):
@@ -22,10 +22,13 @@ def saveSince(since):
     with open(file_name, "w") as f:
         f.write(str(since))
             
-def loadArticles(since):
-    response = requests.get(f'https://getpocket.com/v3/get?consumer_key={consumer_key}&access_token={access_token}&state=archive&detailType=complete&since={since}&sort=oldest&tag=newsletter&count=1').content
+def loadArticles(since, single):
+    url = f'https://getpocket.com/v3/get?consumer_key={consumer_key}&access_token={access_token}&state=archive&detailType=complete&since={since}&sort=oldest&tag=newsletter'
+    
+    if single:
+        url += "&count=1"
 
-    return json.loads(response)
+    return json.loads(requests.get(url).content)
     
 def mapArticles(data):
     items = list(map(lambda kv: kv[1], data["list"].items()))
@@ -56,10 +59,16 @@ if __name__ == "__main__":
     telegram_bot_api_key = sys.argv[3]
 
     since = readSince()
+    
+    print(f"Since read: {since}\n")
 
     bot = telegram.Bot(token=telegram_bot_api_key)
 
-    data = loadArticles(since)
+    allArticles = loadArticles(since, False)
+    print(f'All articles:\n {json.dumps(allArticles).encode("utf-8")}\n')
+    
+    data = loadArticles(since, True)
+    print(f'Articles to post:\n {json.dumps(data).encode("utf-8")}\n')
 
     if 'list' in data and len(data["list"]) > 0:
         items = mapArticles(data)
@@ -67,8 +76,13 @@ if __name__ == "__main__":
         for item in items:
             try:
                 bot.send_message(chat_id="@krossovochkin_newsletter", text=item[0], parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
-                # print(item[0].encode("utf-8"))
-                # print(item[1])
-                saveSince(item[1])
+                
+                message = item[0].encode("utf-8")
+                print(f"Message sent: \n{message}\n")
+
+                since = item[1]
+                saveSince(since)
+                
+                print(f"Since saved: {since}")
             except Exception as e:
                 print(f"Error: {e}")
